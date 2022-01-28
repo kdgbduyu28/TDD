ab74f516339ac40bddd08fd8156e929c37b68183
Timothy Borja <kdgbduyu28@gmail.com>
2022-01-28 15:04:22 +0800

    Setup test action2

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,18 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-
-import React from 'react';
-import { mount } from '@cypress/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders learn react link', () => {
-  mount(<App />);
-  cy.get('a').contains('Learn React');
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
+
+
+// import React from 'react';
+// import { mount } from '@cypress/react';
+// import App from './App';
+
+// it('renders learn react link', () => {
+//   mount(<App />);
+//   cy.get('a').contains('Learn React');
+// });
