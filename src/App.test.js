a83edcb165204502c76eae3842b5024d93716d64
Timothy Borja <kdgbduyu28@gmail.com>
2022-01-30 13:48:01 +0800

    Setup test action4

--- src/App.test.js
+++ src/App.test.js
@@ -9,10 +9,12 @@
 
 
 import React from 'react';
-import { mount } from '@cypress/react';
+import mount from '@cypress/react';
 import App from './App';
 
+describe('HelloWorld component', () => {
 it('renders learn react link', () => {
   mount(<App />);
   cy.get('a').contains('Learn React');
+})
 });
